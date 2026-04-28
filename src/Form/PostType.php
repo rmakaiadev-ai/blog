f1b3c0574c6acc05589874849ce46d4dadd1ae75
Randy <rmakaiadevgmail.com>
2026-04-28 11:10:54 +0200

    Ajoute de crud category et post

---
+++ src/Form/PostType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('author')
+            ->add('publishedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('isFeatured')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
